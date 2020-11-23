13e574f997a2d9f68606a56e5726e8db3d8433ce
city1616 <city1616@naver.com>
2020-11-23 20:00:19 +0900

    수정

--- CampusMaps/ContentView.swift
+++ CampusMaps/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    
     var body: some View {
         return ARViewContainer().edgesIgnoringSafeArea(.all)
         // NaverMapView().edgesIgnoringSafeArea(.all)
@@ -17,23 +18,23 @@ struct ContentView : View {
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    
+
     func makeUIView(context: Context) -> ARView {
-        
+
         let arView = ARView(frame: .zero)
-        
+
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
-        
+
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
-        
+
         return arView
-        
+
     }
-    
+
     func updateUIView(_ uiView: ARView, context: Context) {}
-    
+
 }
 
 #if DEBUG
