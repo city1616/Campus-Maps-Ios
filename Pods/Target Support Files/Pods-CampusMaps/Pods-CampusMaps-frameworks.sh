0a60509e8469a212f52ab5bfc61eede1a72c88d0
city1616 <city1616@naver.com>
2020-11-30 00:41:03 +0900

    Pods 수정 & 추가

--- Pods/Target Support Files/Pods-CampusMaps/Pods-CampusMaps-frameworks.sh
+++ Pods/Target Support Files/Pods-CampusMaps/Pods-CampusMaps-frameworks.sh
@@ -175,9 +175,13 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${PODS_ROOT}/Mapbox-iOS-SDK/dynamic/Mapbox.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MapboxMobileEvents/MapboxMobileEvents.framework"
   install_framework "${PODS_ROOT}/NMapsMap/framework/NMapsMap.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${PODS_ROOT}/Mapbox-iOS-SDK/dynamic/Mapbox.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MapboxMobileEvents/MapboxMobileEvents.framework"
   install_framework "${PODS_ROOT}/NMapsMap/framework/NMapsMap.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
