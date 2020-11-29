c56a7dccc1d59fc9078803cc3eb987031a2c9a93
city1616 <city1616@naver.com>
2020-11-29 18:24:33 +0900

    Side Menu 생성

--- CampusMaps/ContentView.swift
+++ CampusMaps/ContentView.swift
@@ -16,81 +16,105 @@ import ARKit
 //}
 struct ContentView : View {
 
-    @State var ShowMenu = false
+    @State var SideMenu = false
     @State var arToggle = false
+    
+    
 
     var body: some View {
-
-        NavigationView {
-            VStack() {
-                // Text("AR")
-                NaverMapView().edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                // ARViewContainer().edgesIgnoringSafeArea(.all)
-                VStack(alignment: .center, spacing: 0.0) {
-                    HStack() {
-                        Spacer()
-                        Button(action: {}) {
-                            VStack {
-                                Image(systemName: "map")
-                                    .font(.title3)
-                                Text("MAP")
-                                    .multilineTextAlignment(.center)
-                                    .font(.footnote)
-                            }
-                        }
-                        Spacer()
-                        Button(action: {}) {
-                            VStack {
-                                // Image(systemName: "mappin.and.ellipse")
-                                Image(systemName: "location.circle")
-                                    .font(.title3)
-                                Text("NAV")
-                                    .font(.footnote)
-                            }
-                        }
-                        Spacer()
-                        Button(action: {
-                            withAnimation {
-                                self.arToggle.toggle()
-                            }
-                        }) {
-                            VStack {
-                                // Image(systemName: "camera.on.rectangle")
-                                Image(systemName: "arkit")
-                                    .font(.title3)
-                                Text("AR")
-                                    .font(.footnote)
-                                    .sheet(isPresented: $arToggle) {
-                                        VStack {
-                                            ARViewContainer().edgesIgnoringSafeArea(.all)
-//                                            NaverMapView().edgesIgnoringSafeArea(.all)
-//                                                .frame(width: .zero, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                        }
-                                    }
-                            }
-                        }
-                        Spacer()
-                        Button(action: {}) {
-                            VStack {
-                                Image(systemName: "person.circle")
-                                    .font(.title3)
-                                Text("MY")
-                                    .font(.footnote)
-                            }
-                        }
-                        Spacer()
+        
+        let drag = DragGesture()
+            .onEnded {
+                if $0.translation.width < -100 {
+                    withAnimation {
+                        self.SideMenu = false
                     }
-                    .padding(.top, 10.0)
+                }
+            }
+
+        return NavigationView {
+            ZStack(alignment: .leading) {
+                
+                VStack() {
+                    // Text("AR")
+                    NaverMapView().edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    // ARViewContainer().edgesIgnoringSafeArea(.all)
+                    VStack(alignment: .center, spacing: 0.0) {
+                        HStack() {
+                            Spacer()
+                            Button(action: {}) {
+                                VStack {
+                                    Image(systemName: "map")
+                                        .font(.title3)
+                                    Text("MAP")
+                                        .multilineTextAlignment(.center)
+                                        .font(.footnote)
+                                }
+                            }
+                            Spacer()
+                            Button(action: {}) {
+                                VStack {
+                                    // Image(systemName: "mappin.and.ellipse")
+                                    Image(systemName: "location.circle")
+                                        .font(.title3)
+                                    Text("NAV")
+                                        .font(.footnote)
+                                }
+                            }
+                            Spacer()
+                            Button(action: {
+                                withAnimation {
+                                    self.arToggle.toggle()
+                                }
+                            }) {
+                                VStack {
+                                    // Image(systemName: "camera.on.rectangle")
+                                    Image(systemName: "arkit")
+                                        .font(.title3)
+                                    Text("AR")
+                                        .font(.footnote)
+                                        .sheet(isPresented: $arToggle) {
+                                            VStack {
+                                                ARViewContainer().edgesIgnoringSafeArea(.all)
+    //                                            NaverMapView().edgesIgnoringSafeArea(.all)
+    //                                                .frame(width: .zero, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                            }
+                                        }
+                                }
+                            }
+                            Spacer()
+                            Button(action: {}) {
+                                VStack {
+                                    Image(systemName: "person.circle")
+                                        .font(.title3)
+                                    Text("MY")
+                                        .font(.footnote)
+                                }
+                            }
+                            Spacer()
+                        }
+                        .padding(.top, 10.0)
+                    }
+                }
+                    .offset(x: self.SideMenu ? UIScreen.main.bounds.width / 2 : 0)
+                    .disabled(self.SideMenu ? true : false)
+                
+                if self.SideMenu {
+                    CampusMaps.SideMenu()
+                        // .frame(width: geo.size.width / 2)
+                        .frame(width: UIScreen.main.bounds.width / 2)
+                        .transition(.move(edge: .leading))
+                        .edgesIgnoringSafeArea(.top)
                 }
             }
             .navigationTitle("Campus Maps")
             .navigationBarTitleDisplayMode(.inline)
             .navigationBarItems(leading: Button(action: {
                 withAnimation {
-                    self.ShowMenu.toggle()
+                    self.SideMenu.toggle()
                 }
             }) {
-                Text("menu")
+                Text("Menu")
             }, trailing: Button(action: {
                 withAnimation {
                     self.arToggle.toggle()
@@ -99,6 +123,7 @@ struct ContentView : View {
                 Image(systemName: "ellipsis")
                     .font(.title3)
             })
+            .gesture(drag)
         }
 //         .edgesIgnoringSafeArea(.all)
 //
