adff16e0b2492397674eea1d7d472d474094a927
city1616 <city1616@naver.com>
2020-12-09 21:49:03 +0900

    add 3D Object

---
+++ CampusMaps/ObjectView.swift
@@ -0,0 +1,20 @@
+//
+//  ObjectView.swift
+//  CampusMaps
+//
+//  Created by SeungWoo Mun on 2020/12/09.
+//
+
+import SwiftUI
+
+struct ObjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ObjectView_Previews: PreviewProvider {
+    static var previews: some View {
+        ObjectView()
+    }
+}
---
+++ CampusMaps/Tabview/ARNaviView.swift
@@ -0,0 +1,20 @@
+//
+//  ARNaviView.swift
+//  CampusMaps
+//
+//  Created by SeungWoo Mun on 2020/12/09.
+//
+
+import SwiftUI
+
+struct ARNaviView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ARNaviView_Previews: PreviewProvider {
+    static var previews: some View {
+        ARNaviView()
+    }
+}
