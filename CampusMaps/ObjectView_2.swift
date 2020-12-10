9bb740616044693f890cbfb4e138a6baee856ab5
city1616 <city1616@naver.com>
2020-12-10 17:57:52 +0900

    ObjectView_2 추가, SelectDestinationView 추가

---
+++ CampusMaps/ObjectView_2.swift
@@ -0,0 +1,20 @@
+//
+//  ObjectView_2.swift
+//  CampusMaps
+//
+//  Created by SeungWoo Mun on 2020/12/10.
+//
+
+import SwiftUI
+
+struct ObjectView_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ObjectView_2_Previews: PreviewProvider {
+    static var previews: some View {
+        ObjectView_2()
+    }
+}
---
+++ CampusMaps/Tabview/SelectDestinationView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectDestinationView.swift
+//  CampusMaps
+//
+//  Created by SeungWoo Mun on 2020/12/10.
+//
+
+import SwiftUI
+
+struct SelectDestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectDestinationView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectDestinationView()
+    }
+}
